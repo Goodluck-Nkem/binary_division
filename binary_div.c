5e613e73416e44c62103b5bb3cfeac6af236212d
Goodluck Ogbuehi <goodluckogbuehi@gmail.com>
2023-10-30 20:14:42 +0100

    Fixed remainder and more
    In addition to fixing the remainder calculation, I added printing action for both quotient and reminder, binary_div will now return a struct containing both

--- binary_div.c
+++ binary_div.c
@@ -5,7 +5,12 @@
 #define MSB_NUMBER ((sizeof(uint32_t) * 8) - 1)
 #define MSB_MASK (1 << MSB_NUMBER)
 
-uint32_t binary_div(uint32_t dividend, uint32_t divisor);
+struct DIV_RESULT
+{
+    uint32_t quotient, remainder;
+};
+
+struct DIV_RESULT binary_div(uint32_t dividend, uint32_t divisor);
 
 int main()
 {
@@ -18,22 +23,24 @@ int main()
         scanf("%u / %u", &dividend, &divisor);
         if (!divisor)
             break;
-        printf("Quotient = %i\n", binary_div(dividend, divisor));
+        struct DIV_RESULT div = binary_div(dividend, divisor);
+        printf("Quotient = %u\nRemainder = %u\n", div.quotient, div.remainder);
     }
 
     puts("Exit success!");
     return 0;
 }
 
-uint32_t binary_div(uint32_t dividend, uint32_t divisor)
+struct DIV_RESULT binary_div(uint32_t dividend, uint32_t divisor)
 {
+    struct DIV_RESULT res = {0, 0};
     uint32_t quotient = 0;
     uint32_t dividend_shift, divisor_msb = MSB_NUMBER, dividend_msb = MSB_NUMBER; // starting from MSB
     uint32_t temp_divisor = divisor;
     uint32_t temp_dividend = dividend;
     uint32_t new_dividend;
     if (!divisor || divisor > dividend)
-        return 0;
+        return res;
 
     while ((temp_divisor & MSB_MASK) == 0)
     {
@@ -63,7 +70,13 @@ uint32_t binary_div(uint32_t dividend, uint32_t divisor)
     }
     quotient <<= 1;
     if (new_dividend >= divisor)
+    {
+        new_dividend -= divisor;
         quotient |= 1;
-    // new_dividend is our remainder
-    return quotient;
+    }
+
+    // we have our quotient, then new_dividend is our remainder
+    res.quotient = quotient;
+    res.remainder = new_dividend;
+    return res;
 }
